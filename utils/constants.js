6b0b3443db028602949b94dc6b5c518ed1a8a79c
Anastasia Aleksashkina <aleksashkina.nastya@yandex.ru>
2022-11-28 18:04:21 +0300

    refactor: обновить README.md

--- utils/constants.js
+++ utils/constants.js
@@ -3,6 +3,6 @@ module.exports.MONGO_DB_CODE = 11000;
 module.exports.REGEXP_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 module.exports.allowedCors = [ // Домены, с которых разрешены кросс-доменные запросы
   'http://localhost:3000',
-  'https://aleksashkina.nomoredomains.icu',
-  'http://aleksashkina.nomoredomains.icu',
+  'https://aleksashkina.movies.nomoredomains.club',
+  'http://aleksashkina.movies.nomoredomains.club',
 ];
